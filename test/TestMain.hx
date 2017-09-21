741cc381a4f0e321dcc8ec41ba04082495ee1ba5
Thomas Uster <thomasuster@gmail.com>
2017-09-20 20:13:39 -0700

    Add multiple runs for flakiness

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -2,11 +2,22 @@ import haxe.unit.TestRunner;
 class TestMain {
 
     static function main() {
+        var times:Int = 1; 
+        if(Sys.args().length > 0)
+          times = Std.parseInt(Sys.args()[0]);
         var runner:TestRunner = new TestRunner();
         runner.add(new TestPool());
-        if(runner.run())
-            Sys.exit(0);
-        Sys.exit(1);
+        for (i in 0...times) {
+          if(!runner.run()) {
+            if(times > 1)
+              Sys.println('FAILURE: ${i+1}/$times');
+            Sys.exit(1);
+          }
+        }
+        if(times > 1)
+          Sys.println('SUCCESS: $times/$times');
+        Sys.exit(0);
+        
     }
 
 }
