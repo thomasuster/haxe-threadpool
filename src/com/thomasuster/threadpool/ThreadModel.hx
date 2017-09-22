4419fdc1d6b4de35da3142dbdb798e68b5a176f3
Endes <endes123321@gmail.com>
2017-09-22 16:38:14 +0100

    Retrying to rebase.

--- src/com/thomasuster/threadpool/ThreadModel.hx
+++ src/com/thomasuster/threadpool/ThreadModel.hx
@@ -1,18 +1,34 @@
 package com.thomasuster.threadpool;
 #if cpp
-import cpp.vm.Mutex;
-#else
-import neko.vm.Mutex;
+import com.thomasuster.threadpool.impl.MutexCpp;
+#elseif neko
+import com.thomasuster.threadpool.impl.MutexN;
+#elseif java
+import com.thomasuster.threadpool.impl.MutexJ;
+#elseif cs
+import com.thomasuster.threadpool.impl.MutexCs;
+#elseif python
+import com.thomasuster.threadpool.impl.MutexPy;
 #end
 class ThreadModel {
     public var id:Int;
     public var start:Int;
     public var end:Int;
-    public var mutex:Mutex;
+		public var mutex:MutexInterface;
     public var pending:Bool;
     public var done:Bool;
 
     public function new():Void {
-        mutex = new Mutex();
+      #if cs
+      mutex = new MutexCs();
+      #elseif python
+      mutex = new MutexPy();
+      #elseif cpp
+      mutex = new MutexCpp();
+      #elseif java
+      mutex = new MutexJ();
+      #elseif neko
+      mutex = new MutexN();
+      #end
     }
 }
---
+++ src/com/thomasuster/threadpool/impl/MutexPy.hx
@@ -0,0 +1,11 @@
+package com.thomasuster.threadpool.impl;
+
+import python.lib.threading.Lock;
+
+class MutexPy extends Lock implements MutexInterface {
+
+  public function new() {
+    super();
+  }
+
+}
